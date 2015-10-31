ae27784fa86a8bafb60cf70d4cd192488640eed3
Hynek Schlawack <hs@ox.cx>
2015-10-31 08:24:54 +0100

    bump in dev version

--- src/prometheus_async/__init__.py
+++ src/prometheus_async/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 import six
 
 
-__version__ = "15.0.0.dev0"
+__version__ = "15.0.0.dev1"
 
 __title__ = "prometheus_async"
 __description__ = "Async helpers for prometheus_client."
