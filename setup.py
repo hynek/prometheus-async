6e139f7ed18157aea015ac6b0fe52860446d5c2f
Hynek Schlawack <hs@ox.cx>
2016-02-17 11:52:34 +0100

    Drop Python 2.6

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ CLASSIFIERS = [
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.4",
