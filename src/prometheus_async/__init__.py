e63374bc1fb3ac3eb678432d09b249a7a03e701a
Hynek Schlawack <hs@ox.cx>
2016-05-19 17:20:04 +0200

    Prepare 16.0

--- src/prometheus_async/__init__.py
+++ src/prometheus_async/__init__.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import, division, print_function
 import six
 
 
-__version__ = "16.0.0.dev5"
+__version__ = "16.0.0"
 
 __title__ = "prometheus_async"
 __description__ = "Async helpers for prometheus_client."
