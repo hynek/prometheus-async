976d380ae1fa6c20dd994faada1fd3cd53569f4b
Tin Tvrtković <tinchester@gmail.com>
2022-01-13 14:51:16 +0100

    Add typing_examples.py

---
+++ typing_examples.py
@@ -0,0 +1,32 @@
+"""Some examples of prometheus-async typing integration."""
+from asyncio import Future
+
+from prometheus_client.metrics import Summary  # type: ignore
+
+from prometheus_async.aio import time
+
+
+REQ_DURATION = Summary("REQ_DUR", "Request duration")
+
+
+@time(REQ_DURATION)
+async def func(i: int) -> str:
+    return str(i)
+
+
+# The type of `func` is correct: "def (i: builtins.int) -> typing.Coroutine*[Any, Any, builtins.str]"
+reveal_type(func)
+
+
+@time(REQ_DURATION)
+def future_func(i: int) -> Future[str]:
+    return Future()
+
+
+# The type of `future_func` is correct: "def (i: builtins.int) -> asyncio.futures.Future*[builtins.str]"
+reveal_type(future_func)
+
+# The following is a type error since the function cannot be awaited:  Value of type variable "R" of function cannot be "str"
+@time(REQ_DURATION)
+def should_be_async_func(i: int) -> str:
+    return str(i)
