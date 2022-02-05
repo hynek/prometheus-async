f6c80af17435ce27e933f0b70bb741e445ad0849
Hynek Schlawack <schlawack@variomedia.de>
2022-02-05 12:01:11 +0100

    Add some Twisted examples

--- typing_examples.py
+++ typing_examples.py
@@ -5,14 +5,15 @@ Some examples of prometheus-async typing integration.
 from asyncio import Future
 
 from prometheus_client.metrics import Summary
+from twisted.internet.defer import Deferred
 
-from prometheus_async.aio import time
+from prometheus_async import aio, tx
 
 
 REQ_DURATION = Summary("REQ_DUR", "Request duration")
 
 
-@time(REQ_DURATION)
+@aio.time(REQ_DURATION)
 async def func(i: int) -> str:
     return str(i)
 
@@ -22,24 +23,24 @@ async def func(i: int) -> str:
 # reveal_type(func)
 
 
-@time(REQ_DURATION)
+@aio.time(REQ_DURATION)
 def future_func(i: int) -> Future[str]:
     return Future()
 
 
 # `time` can also be applied to futures directly.
 future = Future[str]()
-time(REQ_DURATION, future)
+aio.time(REQ_DURATION, future)
 
 
 async def coro() -> None:
     pass
 
 
-time(REQ_DURATION, coro())  # `time` can also be applied to coroutines
+aio.time(REQ_DURATION, coro())  # `time` can also be applied to coroutines
 
-time(REQ_DURATION, coro)  # type: ignore  # `time` errors on coroutine fns
-time(REQ_DURATION, int)  # type: ignore # `time` errors on non-futures
+aio.time(REQ_DURATION, coro)  # type: ignore  # `time` errors on coroutine fns
+aio.time(REQ_DURATION, int)  # type: ignore # `time` errors on non-futures
 
 # The type of `future_func` is correct:
 # "def (i: builtins.int) -> asyncio.futures.Future*[builtins.str]"
@@ -49,6 +50,17 @@ time(REQ_DURATION, int)  # type: ignore # `time` errors on non-futures
 # "def (i: builtins.int) -> asyncio.futures.Future*[builtins.str]"
 
 
-@time(REQ_DURATION)  # type: ignore
+@aio.time(REQ_DURATION)  # type: ignore
 def should_be_async_func(i: int) -> str:
     return str(i)
+
+
+#
+# Twisted
+#
+tx.time(REQ_DURATION, Deferred())
+
+
+@tx.time(REQ_DURATION)
+def returns_deferred() -> Deferred:
+    return Deferred()
